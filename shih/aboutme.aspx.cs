f7c0c7b34b521aebcea927b66e59da43be83c45a
PStar <pstar.lin@mail.im.tku.edu.tw>
2014-02-07 17:41:58 +0800

    [shih] 新增學歷xml檔

--- shih/aboutme.aspx.cs
+++ shih/aboutme.aspx.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 public partial class aboutme : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        XmlDocument xmlDocEdu = new XmlDocument();
+
+        xmlDocEdu.Load(Server.MapPath("xmlDB/Education.xml"));
 
     }
 }
